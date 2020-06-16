d2a9ff14138c0b73d2746209fcd3066040ef3cd2
marasinipradeep <marasinipradeep@gmail.com>
2020-06-16 13:04:43 +0930

    #Working on to get values of buttons

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,84 +1,125 @@
-var jumbtronEl = $("#jumbotronBox")
-var currentDateEl = $("#currentDay")
-var timeBlocksEl = $("#timeBlocks")
+$(document).ready(function () {
 
-init();
-renderTimeBlock();
+    var jumbtronEl = $("#jumbotronBox")
+    var currentDateEl = $("#currentDay")
+    var timeBlocksEl = $("#timeBlocks")
 
-function init() {
-    var gatDate = getTodaysDate();
-    jumbtronEl.text("hello day planner")
-    currentDateEl.text(gatDate)
-}
+    init();
 
-function getTodaysDate() {
-    var d = new Date();
-    var day = d.getDate();
+    var dayPlanner = {
+        timeSlots: [9, 10, 11, 12, 1, 2, 3, 4, 5],
+        timeType: ["AM", "PM"],
+        listOfTask: [],
+        timeWithTimeType: function () {
+            var newTimeSlot = []
+            for (var i = 0; i < 4; i++) {
+                var newTimeAM = this.timeSlots[i] + this.timeType[0]
+                newTimeSlot.push(newTimeAM)
+            }
+            for (var j = 4; j < this.timeSlots.length; j++) {
+                var newTimePM = this.timeSlots[j] + this.timeType[1]
+                newTimeSlot.push(newTimePM)
+            }
+            console.log(newTimeSlot)
+            return newTimeSlot
+        }
+    }
 
-    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var week = weekdays[d.getDay()]
 
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var month = months [d.getMonth()];
+    /* Displaying UI (UserInterface) which has time slots, text area for inputs and save button for all */
+
+
+   $.each(dayPlanner.timeWithTimeType(), function (index, item) {
+        var blockDiv = $("<div>")
+        blockDiv.addClass("row")
+        blockDiv.data("number", index)
+       // console.log("I am row : " + blockDiv.data("number"))
+        var time = $("<h3>").addClass("col-3 hour").text(item)
+        var schedule = $("<input>").addClass("col-8 future").data("number", index)
+
+        var saveButton = $("<button>")
+        saveButton.addClass("col-1 saveBtn fa fa-save")
+        saveButton.data("number", index)
+       // saveButton.attr("id","button-"+index)
+        console.log("I am save Button : " + saveButton.data("number"))
+        blockDiv.append(time, schedule, saveButton)
+        timeBlocksEl.append(blockDiv)
+
+    })
+
+    $(".saveBtn").each(function(event) {
+        
+       // event.preventDefault()
+       
+       // var getButtonValue = $("button").data("number");
+
+        console.log("inside save button clicked :"+$("button").data("number"))
+
+      //  localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner))
+
+    })
+
+
+
+    function init() {
+        var gatDate = getTodaysDate();
+        currentDateEl.text(gatDate)
+    }
+
+    function getTodaysDate() {
+        var d = new Date();
+        var day = d.getDate();
+
+        var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var week = weekdays[d.getDay()]
+
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var month = months[d.getMonth()];
+
+
+        var output = week + ", " + month + " " +
+            (day < 10 ? '0' : '') + ordinal_suffix_of(day)
+        return output
+    }
+    /*Creating  suffix to addd on date day */
+    function ordinal_suffix_of(i) {
+        var j = i % 10,
+            k = i % 100;
+        if (j == 1 && k != 11) {
+            return i + "st";
+        }
+        if (j == 2 && k != 12) {
+            return i + "nd";
+        }
+        if (j == 3 && k != 13) {
+            return i + "rd";
+        }
+        return i + "th";
+    }
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        var strTime = hours + ampm;
+        return strTime;
+    }
+
     
 
-    var output = week + ", " + month + " " +
-    (day < 10 ? '0' : '') + ordinal_suffix_of(day)
-    return output
-}
+    function getListOfAllDayPlanning() {
 
-/*Creating  suffix to addd on date day */
-function ordinal_suffix_of(i) {
-    var j = i % 10,
-        k = i % 100;
-    if (j == 1 && k != 11) {
-        return i + "st";
+        var messageZero = localStorage.getItem("storeMessageZero")
+        var messageOne = localStorage.getItem("storeMessageOne")
+        // var getDatAttribute = $("textarea");
+        // getDatAttribute = $(this).val();
+        // if(getDatAttribute==="0"){
+        //     console.log(getDatAttribute)
+        // $('textarea').html(messageZero)
+        // }
+        $('textarea').html(messageZero, messageOne)
+
+
     }
-    if (j == 2 && k != 12) {
-        return i + "nd";
-    }
-    if (j == 3 && k != 13) {
-        return i + "rd";
-    }
-    return i + "th";
-}
 
-
-
-function formatAMPM(date) {
-    var hours = date.getHours();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    var strTime = hours  + ampm;
-    return strTime;
-  }
-  
-function renderTimeBlock(event) {
-   
-
-    var times = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"]
-    var presentTime = formatAMPM(new Date)
-    times.forEach(function (item, index){
-    var blockDiv = $("<div>")
-    blockDiv.addClass("row")
-    var time = $("<h3>").addClass("col-2 hour").text(item)
-    console.log("item : "  +item + " index : "  +index)
-  
-    console.log("present time : " +presentTime)
-
-    var schedule = $("<textarea>").addClass("col-8 ")
-
-    if(times[0] === "9AM"){
-        schedule.addClass("future")
-     }
-     
-    var save = $("<button>").addClass("col-2 saveBtn fa fa-save")
-    blockDiv.append(time, schedule, save)
-    $("#timeBlocks").append(blockDiv)
-})
-}
-
-
-
-
+});
