56dab95bab469e434ed9cbdf91637008c4072245
marasinipradeep <marasinipradeep@gmail.com>
2020-06-17 11:28:16 +0930

    #Stored value of dayPlanner object on local storage

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -7,8 +7,45 @@ $(document).ready(function () {
 
     var dayPlanner = {
         timeSlots: ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"],
-        time:[9, 10, 11, 12, 13, 14, 15, 16, 17],
-        listOfTask: [],
+        time: [9, 10, 11, 12, 13, 14, 15, 16, 17],
+        listOfTask: [
+            {
+                workTime: "9AM",
+                task: ""
+            },
+            {
+                workTime: "10AM",
+                task: ""
+            },
+            {
+                workTime: "11AM",
+                task: ""
+            },
+            {
+                workTime: "12AM",
+                task: ""
+            },
+            {
+                workTime: "1PM",
+                task: ""
+            },
+            {
+                workTime: "2PM",
+                task: ""
+            },
+            {
+                workTime: "3PM",
+                task: ""
+            },
+            {
+                workTime: "4PM",
+                task: ""
+            },
+            {
+                workTime: "5PM",
+                task: ""
+            },
+        ]
     }
 
 
@@ -16,8 +53,14 @@ $(document).ready(function () {
         var currentDate = moment().format('dddd, MMMM Do');
         currentDateEl.text(currentDate)
 
-       // saveDayPlanner();
-       
+        var newDayPlannerObject = JSON.parse(localStorage.getItem("dayPlanner"))
+
+        if (newDayPlannerObject) {
+            dayPlanner = newDayPlannerObject;
+        }
+
+        saveDayPlanner();
+        populateDayPlanner();
     }
 
 
@@ -31,23 +74,24 @@ $(document).ready(function () {
         var time = $("<h3>").addClass("col-3 hour").text(item)
         time.data("number", item)
 
-        var schedule = $("<input/>")
+        var schedule = $("<textarea/>")
         schedule.addClass("col-8")
+        schedule.data("textAreaRow", index)
         if (dayPlanner.time[index] < moment().format("HH")) {
-           
+
             schedule.addClass("past")
         }
-       else if (dayPlanner.time[index] == moment().format("HH")) {
-            
+        else if (dayPlanner.time[index] == moment().format("HH")) {
+
             schedule.addClass("present")
         }
 
-        else if(dayPlanner.time[index]>moment().format("HH")){
-            
+        else if (dayPlanner.time[index] > moment().format("HH")) {
+
             schedule.addClass("future")
         }
 
-       
+
         var saveButton = $("<button>")
         saveButton.addClass("col-1 saveBtn fa fa-save")
         saveButton.data("number", index + 1)
@@ -58,34 +102,34 @@ $(document).ready(function () {
         blockDiv.append(time, schedule, saveButton)
         timeBlocksEl.append(blockDiv)
 
-        populateDayPlanner(schedule);
-
-      
     })
 
     function onButtonClickes(time, schedule, saveButton) {
 
         event.preventDefault()
-        var onSaveButtonClicked = saveButton.data("number");
-        var getInputValue = schedule.val();
-        var getTime = time.data("number")
-
-        if (onSaveButtonClicked) {
-            dayPlanner.listOfTask.push(getInputValue)
-        }
+        var textAreaIndex = schedule.data("textAreaRow");
+        dayPlanner.listOfTask[textAreaIndex].task = schedule.val();
         saveDayPlanner();
+        populateDayPlanner(schedule);
     }
 
 
-    function saveDayPlanner(){
-
+    function saveDayPlanner() {
         localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner))
-
     }
-    
+
     function populateDayPlanner(schedule) {
-        var newDayPlannerObject = JSON.parse(localStorage.getItem("dayPlanner"))
-        console.log(newDayPlannerObject.listOfTask[0])
-        schedule.val(newDayPlannerObject.listOfTask[0])
+
+
+        $.each(dayPlanner.listOfTask, function (index, item,) {
+
+            // console.log((item.workTime))
+            // console.log((item.task))
+
+            // item.workTime.val(item.task);
+            
+
+        })
     }
 });
+
